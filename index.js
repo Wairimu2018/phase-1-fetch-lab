67d036de5c8f37d0f599caac2396d90e6b06241b
Wairimu2018 <you@example.com>
2022-06-15 15:18:17 +0300

    My commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch!
+  function fetchBooks(books) {
+    return fetch('https://anapioficeandfire.com/api/books')
+    .then (resp => resp.json())
+    .then(json => renderBooks(json))
   
 }
 
@@ -15,3 +19,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
